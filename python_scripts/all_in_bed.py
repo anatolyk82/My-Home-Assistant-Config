b6b3414202d35e85db20458c476feea488515b50
anatolyk82 <anatoly.k.82@gmal.com>
2018-12-19 17:50:47 +0100

    Add a new sensor 'all_in_bed' with a python script

---
+++ python_scripts/all_in_bed.py
@@ -0,0 +1,13 @@
+
+all_in_bed = True
+
+for entity_id in hass.states.entity_ids('binary_sensor'):
+    if entity_id.find('motion_sensor') >= 0:
+        state = hass.states.get(entity_id)
+        if state.state == 'on':
+            all_in_bed = False
+            break
+
+hass.states.set('binary_sensor.all_in_bed', all_in_bed, {
+    'friendly_name': 'Everyone in bed'
+})
