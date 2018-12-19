fe9374f19688e4e01d2fcc73576147f5e93c46bf
anatolyk82 <anatoly.k.82@gmal.com>
2018-12-19 18:18:51 +0100

    Add automation to unset the all_in_bed sensor

--- python_scripts/all_in_bed.py
+++ python_scripts/all_in_bed.py
@@ -9,5 +9,6 @@ for entity_id in hass.states.entity_ids('binary_sensor'):
             break
 
 hass.states.set('binary_sensor.all_in_bed', all_in_bed, {
-    'friendly_name': 'Everyone in bed'
+    'friendly_name': 'Everyone in bed',
+    'icon': 'mdi:hotel'
 })
