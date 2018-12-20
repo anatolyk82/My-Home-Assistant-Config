9aa79aec33976a4c91d37ba795c4d08ab01ddf00
anatolyk82 <anatoly.k.82@gmal.com>
2018-12-20 22:32:44 +0100

    Move the entire logic to detect 'all_in_bed' to the python script

--- python_scripts/all_in_bed.py
+++ python_scripts/all_in_bed.py
@@ -1,14 +1,93 @@
 
-all_in_bed = True
+all_in_bed = 'on'
 
+# Check the alarm state
+alarm_state = hass.states.get('alarm_control_panel.house').state
+if alarm_state == 'armed_away':
+    all_in_bed = 'off'
+    logger.debug("AllInBed: Alarm is armed as 'armed_away'. Set the sensor to off")
+
+
+# Check all illumination sensors if their values are below 10
+if all_in_bed == 'on':
+    for entity_id in hass.states.entity_ids('sensor'):
+        if entity_id.find('illumination') >= 0:
+            # Ignore Xiaomi gateway
+            if entity_id.find('7811dcb7f720') >= 0:
+                continue
+            illumination_state = hass.states.get(entity_id)
+            if float(illumination_state.state) > 10:
+                all_in_bed = 'off'
+                logger.debug("AllInBed: Illumination for '%s' is above 10. Set the sensor to off", entity_id)
+                break
+
+
+# Check some lamps if they are on
+# Living room
+if hass.states.get('light.bulb_12').state == 'on' and all_in_bed == 'on':
+    logger.debug("AllInBed: Living room light is on. Set the sensor to off")
+    all_in_bed = 'off'
+
+if hass.states.get('light.bulb_14').state == 'on' and all_in_bed == 'on':
+    logger.debug("AllInBed: Living room light is on. Set the sensor to off")
+    all_in_bed = 'off'
+
+# Entrance
+if hass.states.get('light.bulb_16').state == 'on' and all_in_bed == 'on':
+    logger.debug("AllInBed: Entrance light is on. Set the sensor to off")
+    all_in_bed = 'off'
+
+# Second floor
+if hass.states.get('light.light1').state == 'on' and all_in_bed == 'on':
+    logger.debug("AllInBed: Second floor light is on. Set the sensor to off")
+    all_in_bed = 'off'
+
+# Work room
+if hass.states.get('light.bulb_15').state == 'on' and all_in_bed == 'on':
+    logger.debug("AllInBed: Workroom light is on. Set the sensor to off")
+    all_in_bed = 'off'
+
+if hass.states.get('light.xiaomi_philips_desklamp').state == 'on' and all_in_bed == 'on':
+    logger.debug("AllInBed: Desktop lamp is on. Set the sensor to off")
+    all_in_bed = 'off'
+
+# Bedroom
+if hass.states.get('light.bulb_13').state == 'on' and all_in_bed == 'on':
+    logger.debug("AllInBed: Bedroom light is on. Set the sensor to off")
+    all_in_bed = 'off'
+
+if hass.states.get('light.bed_led').state == 'on' and all_in_bed == 'on':
+    logger.debug("AllInBed: BedLED light is on. Set the sensor to off")
+    all_in_bed = 'off'
+
+# Room
+if hass.states.get('light.bulb_17').state == 'on' and all_in_bed == 'on':
+    logger.debug("AllInBed: Room light is on. Set the sensor to off")
+    all_in_bed = 'off'
+
+
+# Check the media player
+if hass.states.get('media_player.sirius').state == 'on' and all_in_bed == 'on':
+    logger.debug("AllInBed: The media player 'Sirius' is on. Set the sensor to off")
+    all_in_bed = 'off'
+
+
+# Check if there is any motion except the bedroom
 for entity_id in hass.states.entity_ids('binary_sensor'):
     if entity_id.find('motion_sensor') >= 0:
-        state = hass.states.get(entity_id)
-        if state.state == 'on':
-            all_in_bed = False
+        # Ignore motion sensor in the bedroom  
+        if entity_id.find('158d000236a59e') >= 0:
+            continue
+        motion_state = hass.states.get(entity_id)
+        if motion_state.state == 'on':
+            logger.debug("AllInBed: There is motion on %s. Set the sensor to off", entity_id)
+            all_in_bed = 'off'
             break
 
+
+logger.debug("AllInBed: Set binary_sensor.all_in_bed to %s", all_in_bed)
 hass.states.set('binary_sensor.all_in_bed', all_in_bed, {
     'friendly_name': 'Everyone in bed',
     'icon': 'mdi:hotel'
 })
+
