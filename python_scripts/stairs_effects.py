942913e0bd4facd2e41e1f084282ca706eddf773
anatolyk82 <anatoly.k.82@gmal.com>
2019-02-22 21:54:49 +0100

    Add new automation scripts tocontrol the stairs light

---
+++ python_scripts/stairs_effects.py
@@ -0,0 +1,78 @@
+
+motion_entity_id = data.get('entity_id')
+to_state = data.get('to_state')
+#delayed_run = int(data.get('delayed'))
+
+logger.debug("[StairsLight]: %s is turning %s", motion_entity_id, to_state)
+
+# Get information about darkness
+lightlevel_upstairs = float(hass.states.get('sensor.lightlevel_upstairs').state)
+lightlevel_downstairs = float(hass.states.get('sensor.lightlevel_livingroom').state)
+lightlevel_threshold = 5
+is_upstairs_dark = lightlevel_upstairs < lightlevel_threshold 
+is_downstairs_dark = lightlevel_downstairs < lightlevel_threshold
+
+# Get information about last updates on the motion sensors
+motion_upstairs_last_updated = hass.states.get('binary_sensor.motion_upstairs').last_updated
+motion_downstairs_last_updated = hass.states.get('binary_sensor.motion_downstairs').last_updated
+
+# Get the light object
+light_stairs = hass.states.get('light.stairs')
+
+
+def dimDown(hass):
+    service_data = { 'entity_id':'light.stairs', 'effect':'DimDown' }
+    hass.services.call('light', 'turn_on', service_data, False)
+
+def dimUp(hass):
+    service_data = { 'entity_id':'light.stairs', 'effect':'DimUp' }
+    hass.services.call('light', 'turn_on', service_data, False)
+
+# Define light effects
+future_effect_to_on = 'LightUp'
+future_effect_to_off = 'DimUp'
+if motion_entity_id == 'binary_sensor.motion_upstairs':
+    future_effect_to_on = 'LightDown'
+    future_effect_to_off = 'DimDown'
+
+
+# Choose color and brightness
+now = datetime.datetime.now()
+today_00_00 = now.replace(hour=0, minute=0, second=0, microsecond=0)
+today_07_30 = now.replace(hour=6, minute=30, second=0, microsecond=0)
+
+brightness = 255
+rgb_color = [255, 255, 0]
+if now > today_00_00 and now < today_07_30:
+    brightness = 180
+    rgb_color = [255, 0, 0]
+
+
+# Check if it's dark enough to turn on the light
+if to_state == 'on' and is_upstairs_dark and is_downstairs_dark:
+    if light_stairs.state == 'on' and light_stairs.attributes.effect != 'StartLight' and light_stairs.attributes.effect != 'EndLight':
+        logger.debug("[StairsLight]: The light is already on")
+    else:
+        logger.debug("[StairsLight]: Turn on the light")
+        service_data = {
+                'entity_id':'light.stairs',
+                'brightness':brightness,
+                'rgb_color':rgb_color,
+                'effect':future_effect_to_on,
+                }
+        hass.services.call('light', 'turn_on', service_data, False)
+elif to_state == 'on':
+    logger.debug("[StairsLight] It's not dark enough to turn on the light")
+
+
+if to_state == 'off':
+    if light_stairs.state == 'on':
+        if motion_entity_id == 'binary_sensor.motion_downstairs' and motion_downstairs_last_updated > motion_upstairs_last_updated:
+            dimDown(hass)
+
+        elif motion_entity_id == 'binary_sensor.motion_upstairs' and motion_upstairs_last_updated > motion_downstairs_last_updated:
+            dimUp(hass)
+
+
+
+
---
+++ python_scripts/stairs_nightlight.py
@@ -0,0 +1,50 @@
+
+motion_entity_id = data.get('entity_id')
+to_state = data.get('to_state')
+
+#logger.debug("[StairsLight: NightLight]: %s is turning %s", motion_entity_id, to_state)
+
+# Get additional values from sensors
+lightlevel_upstairs = float(hass.states.get('sensor.lightlevel_upstairs').state)
+lightlevel_downstairs = float(hass.states.get('sensor.lightlevel_livingroom').state)
+lightlevel_threshold = 5
+is_upstairs_dark = lightlevel_upstairs < lightlevel_threshold 
+is_downstairs_dark = lightlevel_downstairs < lightlevel_threshold
+
+light_stairs = hass.states.get('light.stairs')
+light_stairs_state = light_stairs.state
+
+# Check current time 
+now = datetime.datetime.now()
+today_00_00 = now.replace(hour=0, minute=0, second=0, microsecond=0)
+today_06_30 = now.replace(hour=6, minute=30, second=0, microsecond=0)
+
+if now > today_00_00 and now < today_06_30:
+    if to_state == 'on' and light_stairs_state == 'off':
+        if motion_entity_id == 'binary_sensor.presence_livingroom' and is_downstairs_dark:
+            logger.debug("[StairsLight: NightLight]: Turn on EndLight")
+            service_data = {
+                    'entity_id':'light.stairs',
+                    'brightness':255,
+                    'rgb_color':[255, 63, 0],
+                    'effect':'EndLight'
+                    }
+            hass.services.call('light', 'turn_on', service_data, False)
+        elif motion_entity_id == 'binary_sensor.presence_livingroom' and not is_downstairs_dark:
+            logger.debug("[StairsLight: NightLight]: Detected motion in the Living room but it's not dark enough")
+        elif motion_entity_id == 'binary_sensor.presence_upstairs' and is_upstairs_dark:
+            logger.debug("[StairsLight: NightLight]: Turn on StartLight")
+            service_data = {
+                    'entity_id':'light.stairs',
+                    'brightness':255,
+                    'rgb_color':[255, 0, 0],
+                    'effect':'StartLight'
+                    }
+            hass.services.call('light', 'turn_on', service_data, False)
+        elif motion_entity_id == 'binary_sensor.presence_upstairs' and not is_upstairs_dark:
+            logger.debug("[StairsLight: NightLight]: Detected motion upstairs but it's not dark enough")
+    elif to_state == 'on' and light_stairs_state == 'on':
+        logger.debug("[StairsLight: NightLight]: Detected motion upstairs but the stairs light is on")
+    elif to_state == 'off' and light_stairs_state == 'on':
+        logger.debug("[StairsLight: NightLight]: Turn off the stairs light")
+        hass.services.call('light', 'turn_off', {'entity_id':'light.stairs'}, False)
