6f05901b78e00f084ef1131976b7c4e90a11b8c9
anatolyk82 <anatoly.k.82@gmal.com>
2020-07-09 23:17:37 +0200

    Extend functionality of blinds_control script
     - Specify a position whne a cover gets open in the morning
     - Specify a position when a cover gets closed in the evening

--- python_scripts/blinds_control.py
+++ python_scripts/blinds_control.py
@@ -1,13 +1,17 @@
 all_covers = [
         {
-            "cover": "cover.blinds_workroom",
-            "enable_switch": "input_boolean.workroom_blinds_auto",
-            "enable_open": True
+            "cover": "cover.blinds_workroom",                              # Blinds id
+            "enable_switch": "input_boolean.workroom_blinds_auto",         # Switch which enables automation for this cover
+            "enable_open": True,                                           # Can the automation open the cover
+            "position_to_open": 25,                                        # Set this position when the cover is open:   100 - fully open
+            "position_to_close": 0                                         # Set this position when the cover is closed: 0 - fully closed
         },
         {
             "cover": "cover.blinds_guestroom",
-            "enable_switch":"input_boolean.guestroom_blinds_auto"
-            "enable_open": True
+            "enable_switch":"input_boolean.guestroom_blinds_auto",
+            "enable_open": True,
+            "position_to_open": 25,
+            "position_to_close": 0
         },    
     ]
 
@@ -18,13 +22,10 @@ def utc2local(utc_td):
     local_td = datetime.datetime.strptime(utc_td, '%Y-%m-%dT%H:%M:%S+00:00') + datetime.timedelta(seconds=offset_sec)
     return local_td
 
-def closeCover(cover_id):
-    service_data = { 'entity_id': cover_id, 'position': 0 }
+def setCoverPosition(cover_id, position):
+    service_data = { 'entity_id': cover_id, 'position': position }
     hass.services.call('cover', 'set_cover_position', service_data, False)
 
-def openCover(cover_id):
-    service_data = { 'entity_id': cover_id, 'position': 100 }
-    hass.services.call('cover', 'set_cover_position', service_data, False)
 
 # Get time
 now = datetime.datetime.now()
@@ -72,8 +73,9 @@ else:
     
 # From which position a cover is considered as closed
 # position = 0 - fully closed; position = 100 - fully open
-thresholdCoverIsClosed = 15
-thresholdCoverIsOpen = 70
+thresholdCoverIsClosed = 100 #20
+thresholdCoverIsOpen = 0 #80
+        
 
 for i in range(0, len(all_covers)):
     cover_id = all_covers[ i ]["cover"]
@@ -93,30 +95,33 @@ for i in range(0, len(all_covers)):
         isOpenningEnabled = all_covers[i]["enable_open"]
         if isOpenningEnabled != True:
             logger.debug("[Blinds Control]: It is not allowed to open blinds %s", cover_id)
-       
+
+        position_to_open = all_covers[i]["position_to_open"]
+        position_to_close = all_covers[i]["position_to_close"]
+
         # Logic to open/close a cover
         if intention == 'open' and isOpenningEnabled == True:
             if intention_info == 'before_sunrise_30' and isCloudy == False and current_position <= thresholdCoverIsClosed and all_in_bed == 'off':
                 logger.debug("[Blinds Control]: The sky is clear. Open the cover %s 30 min before sunrise. Position: %d", cover_id, current_position)
-                openCover(cover_id)
+                setCoverPosition(cover_id, position_to_open)
 
             if intention_info == 'before_sunrise_15' and current_position <= thresholdCoverIsClosed and all_in_bed == 'off':
                 logger.debug("[Blinds Control]: Open the cover %s 15 min before sunrise. Position: %d", cover_id, current_position)
-                openCover(cover_id)
+                setCoverPosition(cover_id, position_to_open)
 
             if intention_info == 'waken_up' and current_position <= thresholdCoverIsClosed and delta_sunrise < 30 and isCloudy == False:
                 logger.debug("[Blinds Control]: The sky is clear, someone has waken up and it's %d minutes until sunrise. Open the cover %s. Position: %d", delta_sunrise, cover_id, current_position)
-                openCover(cover_id)
+                setCoverPosition(cover_id, position_to_open)
 
             if intention_info == 'waken_up' and current_position <= thresholdCoverIsClosed and sun.state == 'above_horizon':
                 logger.debug("[Blinds Control]: Someone has waken up and the sun has risen. Open the cover %s. Position: %d", cover_id, current_position)
-                openCover(cover_id)
+                setCoverPosition(cover_id, position_to_open)
 
         elif intention == 'close':
             if intention_info == 'after_sunset_15' and isCloudy == True and current_position >= thresholdCoverIsOpen:
                 logger.debug("[Blinds Control]: It's cloudy. Close the cover %s 15 min after sunset. Position: %d", cover_id, current_position)
-                closeCover(cover_id)
+                setCoverPosition(cover_id, position_to_close)
 
             if intention == 'close' and intention_info == 'after_sunset_30' and current_position >= thresholdCoverIsOpen:
                 logger.debug("[Blinds Control]: Close the cover %s 30 min after sunset. Position: %d", cover_id, current_position)
-                closeCover(cover_id)
+                setCoverPosition(cover_id, position_to_close)
