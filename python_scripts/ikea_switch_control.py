e09365be9dd27f5b4463bc2ba99140a841d99d78
anatolyk82 <anatoly.k.82@gmal.com>
2020-08-30 21:28:01 +0200

    Prepare the ikea switch control python script to migrate to zigbee2mqtt

--- python_scripts/ikea_switch_control.py
+++ python_scripts/ikea_switch_control.py
@@ -30,23 +30,8 @@ def decrease_value(current_value, delta, min_value):
         return min_value
 
 # Input parameters
-#light_id = data.get('entity_id')
-event = data.get('event')
-
-pos_start = event.find('id=') + 3
-pos_end = event.find(',', pos_start)
-switch_id = event[pos_start:pos_end]
-logger.debug("IKEA Switch Control: switch_id=%s", switch_id)
-
-pos_start = event.find('unique_id=') + 10
-pos_end = event.find(',', pos_start)
-unique_id = event[pos_start:pos_end]
-logger.debug("IKEA Switch Control: unique_id=%s", unique_id)
-
-pos_start = event.find('event=') + 6
-pos_end = event.find(',', pos_start)
-code = event[pos_start:pos_end]
-logger.debug("IKEA Switch Control: code=%s", code)
+switch_id = data.get('switch_id')
+action = data.get('action')
 
 light_id = switch_to_light[switch_id]
 logger.debug("IKEA Switch Control: Light: %s", switch_to_light[switch_id])
@@ -80,45 +65,45 @@ logger.debug("IKEA Switch Control: min_mireds: %s, max_mireds: %s", min_mireds, 
 logger.debug("IKEA Switch Control: brightness: %s, color_temp: %s", brightness, color_temp)
 
 
-if code == "1002":
+if action == "toggle":
     service_data = { 'entity_id': light_id }
     if light.state == 'on':
         hass.services.call('light', 'turn_off', service_data, False)
     else:
         hass.services.call('light', 'turn_on', service_data, False)
-elif code == "2002" and (brightness > -1):
+elif action == "brightness_up_click" and (brightness > -1):
     if light.state == 'on':
         new_brightness = increase_value(brightness, brightness_delta, 255)
         service_data = { 'entity_id': light_id, 'brightness': new_brightness }
         hass.services.call('light', 'turn_on', service_data, False)
-elif code == "3002" and (brightness > -1):
+elif action == "brightness_down_click" and (brightness > -1):
     if light.state == 'on':
         new_brightness = decrease_value(brightness, brightness_delta, 1)
         service_data = { 'entity_id': light_id, 'brightness': new_brightness }
         hass.services.call('light', 'turn_on', service_data, False)
-elif code == "5002" and (color_temp > -1):
+elif action == "arrow_right_click" and (color_temp > -1):
     if light.state == 'on':
         new_color_temp = increase_value(color_temp, color_temp_delta, max_mireds)
         service_data = { 'entity_id': light_id, 'color_temp': new_color_temp }
         hass.services.call('light', 'turn_on', service_data, False)
-elif code == "4002" and (color_temp > -1):
+elif action == "arrow_left_click" and (color_temp > -1):
     if light.state == 'on':
         new_color_temp = decrease_value(color_temp, color_temp_delta, min_mireds)
         service_data = { 'entity_id': light_id, 'color_temp': new_color_temp }
         hass.services.call('light', 'turn_on', service_data, False)
-elif code == "4002" and (color_temp < 0) and (min_mireds > -1):
+elif action == "arrow_left_click" and (color_temp < 0) and (min_mireds > -1):
     if light.state == 'on':
         service_data = { 'entity_id': light_id, 'color_temp': min_mireds }
         hass.services.call('light', 'turn_on', service_data, False)
-elif code == "5002" and (color_temp < 0) and (max_mireds > -1):
+elif action == "arrow_right_click" and (color_temp < 0) and (max_mireds > -1):
     if light.state == 'on':
         service_data = { 'entity_id': light_id, 'color_temp': max_mireds }
         hass.services.call('light', 'turn_on', service_data, False)
-elif code == "2001" and (brightness > -1):
+elif action == "brightness_up_hold" and (brightness > -1):
     if light.state == 'on':
         service_data = { 'entity_id': light_id, 'brightness': 255 }
         hass.services.call('light', 'turn_on', service_data, False)
-elif code == "3001" and (brightness > -1):
+elif action == "brightness_down_hold" and (brightness > -1):
     if light.state == 'on':
         service_data = { 'entity_id': light_id, 'brightness': 1 }
         hass.services.call('light', 'turn_on', service_data, False)
